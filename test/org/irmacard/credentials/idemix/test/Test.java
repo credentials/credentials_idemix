4d45ff4de8347c7e6b3593b381243967e62c3d27
Pim Vullers <pim@vullersmail.nl>
2013-01-10 14:29:52 +0100

    Cleaned up old test

--- test/org/irmacard/credentials/idemix/test/Test.java
+++ test/org/irmacard/credentials/idemix/test/Test.java
@@ -1,5 +1,5 @@
 /**
- * TestSetup.java
+ * Test.java
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,29 +23,24 @@ package org.irmacard.credentials.idemix.test;
 import java.io.File;
 import java.math.BigInteger;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.security.GeneralSecurityException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.Mac;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import javax.smartcardio.CardException;
 import javax.smartcardio.TerminalFactory;
 
+import net.sourceforge.scuba.smartcards.TerminalCardService;
+import net.sourceforge.scuba.smartcards.WrappingCardService;
 import net.sourceforge.scuba.util.Hex;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.irmacard.credentials.util.CardHolderVerificationService;
 import org.irmacard.credentials.util.SecureMessagingWrapper;
+
+import service.IdemixService;
 
 import com.ibm.zurich.credsystem.utils.Locations;
 import com.ibm.zurich.idmx.dm.Values;
@@ -54,25 +49,13 @@ import com.ibm.zurich.idmx.issuance.Issuer;
 import com.ibm.zurich.idmx.issuance.Message;
 import com.ibm.zurich.idmx.key.IssuerKeyPair;
 
-import service.IdemixService;
-
-
-import net.sourceforge.scuba.smartcards.CardService;
-import net.sourceforge.scuba.smartcards.CardServiceException;
-import net.sourceforge.scuba.smartcards.DummyAcceptingCardService;
-import net.sourceforge.scuba.smartcards.TerminalCardService;
-import net.sourceforge.scuba.smartcards.WrappingCardService;
-
 public class Test {
 
-    static {
-        Security.addProvider(new BouncyCastleProvider());
-    }
+    static { Security.addProvider(new BouncyCastleProvider()); }
+    
     private static final IvParameterSpec ZERO_IV_PARAM_SPEC = 
 			new IvParameterSpec(new byte[8]);
-	/**
-	 * @param args
-	 */
+    
 	public static void main(String[] args) {
 		try {
 			Mac mac = Mac.getInstance("DESEDEMAC64WITHISO7816-4PADDING");
@@ -88,12 +71,12 @@ public class Test {
 			byte[] out2 = cipher.doFinal(in2);
 			System.out.println(Hex.toHexString(out2));
 			
-			String credStruct = "CredStructCard4";
-			URI BASE_LOCATION = new File(System.getProperty("user.dir")).toURI().resolve("files/parameter/");
+			String credStruct = "studentCard";
+			URI BASE_LOCATION = new File(System.getProperty("user.dir")).toURI().resolve("irma_configuration/RU/");
 			System.out.println("got base");
-	        URI CRED_LOCATION = BASE_LOCATION.resolve("../issuerData/" + credStruct + ".xml");
+	        URI CRED_LOCATION = BASE_LOCATION.resolve("Issues/structure.xml");
 	        System.out.println("got cred");
-	        URI ISSUER_LOCATION = BASE_LOCATION.resolve("../issuerData/ipk.xml");
+	        URI ISSUER_LOCATION = BASE_LOCATION.resolve("ipk.xml");
 	        System.out.println("got issuer");
 	        URI credStructId = new URI("http://www.ngo.org/" + credStruct + ".xml");
 	        System.out.println("got struct");
@@ -103,9 +86,9 @@ public class Test {
 	        // create the issuance specification
 	        IssuanceSpec issuanceSpec = new IssuanceSpec(ISSUER_LOCATION, credStructId);
 	        System.out.println("got spec");
-	        URI iskLocation = BASE_LOCATION.resolve("../private/isk.xml");
+	        URI iskLocation = BASE_LOCATION.resolve("private/isk.xml");
 	        URI ipkLocation = ISSUER_LOCATION.resolve("ipk.xml");
-	        URI BASE_ID = new URI("http://www.zurich.ibm.com/security/idmx/v2/");
+	        URI BASE_ID = new URI("http://www.irmacard.org/credentials/phase1/RU/");
 	        URI ISSUER_ID = new URI("http://www.issuer.com/");
 	        IssuerKeyPair issuerKey = Locations.initIssuer(BASE_LOCATION, BASE_ID.toString(),
 	                iskLocation, ipkLocation, ISSUER_ID.resolve("ipk.xml"));
@@ -122,9 +105,6 @@ public class Test {
 			// Terminal for communication with the actual card
             TerminalCardService terminal = new TerminalCardService(TerminalFactory.getDefault().terminals().list().get(0));
             CardHolderVerificationService pinpad = new CardHolderVerificationService(terminal);
-            // Dummy in case there is no real card/terminal available
-            @SuppressWarnings("unused")
-			CardService dummy = new DummyAcceptingCardService(System.out);
             
             // Wrapper which performs secure messaging
 			SecureMessagingWrapper sm = new SecureMessagingWrapper(ksMac, ksMac);
@@ -151,28 +131,8 @@ public class Test {
             Message msgToIssuer1 = service.round1(msgToRecipient1);
             Message msgToRecipient2 = issuer.round2(msgToIssuer1);
             service.round3(msgToRecipient2);
-
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		} catch (InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
-		} catch (GeneralSecurityException e) {
-			e.printStackTrace();
-		} catch (CardServiceException e) {
-			e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (CardException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 }
