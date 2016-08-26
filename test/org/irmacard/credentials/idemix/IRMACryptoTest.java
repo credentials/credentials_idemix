fb7b57064cd90a20e6b90cc0a65d4243c58bc50a
Wouter Lueks <wouter@telox.net>
2016-08-26 10:51:27 +0200

    Extracted Attribute test to seperate test-class
    Two reasons for doing this:
    
     1. This is a higher level test, it actually does not test any
     cryptography per se. Instead it focusses on the system
    
     2. It configures the Stores inside the test (rightfully so, they may
     also fail), but this messes up other tests that require a specific
     state of the store. (In particular, reloading the stores reinitializes
     them.)

--- test/org/irmacard/credentials/idemix/IRMACryptoTest.java
+++ test/org/irmacard/credentials/idemix/IRMACryptoTest.java
@@ -663,49 +663,4 @@ public class IRMACryptoTest {
 		ProofD proof = cred.createDisclosureProof(disclosed, context, n_1);
 		assertTrue("Proof of disclosure should verify", proof.verify(pk, context, n_1));
 	}
-
-	@Test
-	public void AttributesTest() throws InfoException, KeyException {
-		URI core = new File(System.getProperty("user.dir")).toURI().resolve("irma_configuration/");
-		DescriptionStore.initialize(new DescriptionStoreDeserializer(core));
-		IdemixKeyStore.initialize(new IdemixKeyStoreDeserializer(core));
-
-		CredentialIdentifier ageLower = new CredentialIdentifier("irma-demo.MijnOverheid.ageLower");
-		Date time = new Date(Calendar.getInstance().getTimeInMillis()
-				/ Attributes.EXPIRY_FACTOR * Attributes.EXPIRY_FACTOR);
-		short keyCounter = 0;
-		short duration = 10;
-
-		Attributes attributes = new Attributes();
-		attributes.add("over12", "yes".getBytes());
-		attributes.add("over16", "yes".getBytes());
-		attributes.add("over18", "no".getBytes());
-		attributes.add("over21", "no".getBytes());
-
-		attributes.setCredentialIdentifier(ageLower);
-		attributes.setKeyCounter(keyCounter);
-		attributes.setValidityDuration(duration);
-		attributes.setSigningDate(time);
-
-		ArrayList<BigInteger> bigints = attributes.toBigIntegers();
-		bigints.add(0, BigInteger.TEN); // secret key
-		CLSignature signature1 = CLSignature.signMessageBlock(sk, pk, bigints);
-		IdemixCredential cred1 = new IdemixCredential(pk, bigints, signature1);
-
-		Random rnd = new Random();
-		IdemixSystemParameters params = pk.getSystemParameters();
-		BigInteger context = new BigInteger(params.get_l_h(), rnd);
-		BigInteger nonce1 = new BigInteger(params.get_l_statzk(), rnd);
-
-		ProofList collection = new ProofListBuilder(context, nonce1)
-				.addProofD(cred1, Arrays.asList(1, 2))
-				.build();
-
-		Attributes disclosed = new Attributes(((ProofD)collection.get(0)).get_a_disclosed());
-		assertTrue("Attributes should be valid", disclosed.isValid());
-		assertTrue("Credential identifiers should match", disclosed.getCredentialIdentifier().equals(ageLower));
-		assertTrue("Key counters should match", disclosed.getKeyCounter() == keyCounter);
-		assertTrue("Validity duration should match", disclosed.getValidityDuration() == duration);
-		assertTrue("Issuing dates should match", disclosed.getSigningDate().equals(time));
-	}
 }
